508e6f6345cb342a26e1727282aa3e088e2ec354
Barry Curtis <barrycurtis@hotmail.com>
2024-07-07 13:05:50 +0100

    Edit syntax

--- main.tf
+++ main.tf
@@ -72,12 +72,8 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
+      target_id = aws_instance.blog.id
+      port = 80
     }
   ]
 
